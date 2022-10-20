3304880c2b1e398153a8abc3fec761f59ccbccaf
dvornick <mail@dvornick.ru>
2022-10-20 18:40:09 +0300

    Add Decorator test

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from decorator import Circle, Square, ColoredShape
 from factory import PointFactory
 from singleton import Database
 
@@ -21,3 +22,27 @@ class FactoryTest(unittest.TestCase):
         point = PointFactory.new_polar_point(5, 5)
         expected = 'x: -4.794621373315692, y: 1.4183109273161312'
         self.assertEqual(str(point), expected)
+
+
+class DecoratorTest(unittest.TestCase):
+    def test_circle(self):
+        circle = Circle(2)
+        expected = 'A circle of radius 2'
+        self.assertEqual(str(circle), expected)
+
+    def test_square(self):
+        square = Square(3)
+        expected = 'A square with side 3'
+        self.assertEqual(str(square), expected)
+
+    def test_colored_circle(self):
+        circle = Circle(2)
+        colored_circle = ColoredShape(circle, 'Red')
+        expected = 'A circle of radius 2 has the color Red'
+        self.assertEqual(str(colored_circle), expected)
+
+    def test_colored_square(self):
+        square = Square(3)
+        colored_square = ColoredShape(square, 'Red')
+        expected = 'A square with side 3 has the color Red'
+        self.assertEqual(str(colored_square), expected)
